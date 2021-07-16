758adb68cc7bbe117e6b1b26243aedc4723c5bb1
yoohyunju <yhj2756@naver.com>
2021-07-17 00:59:34 +0900

    프로젝트 파일

---
+++ app/src/main/java/com/example/bepro/Home.java
@@ -0,0 +1,22 @@
+package com.example.bepro;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Home extends Fragment {
+
+    // @NonNull : null 허용하지 않음
+    // @Nullable : null 허용
+    //onCreateView(): fragment가 자신의 UI를 처음으로 그릴 때 호출됨
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home, container, false);
+
+    }
+}
---
+++ app/src/main/java/com/example/bepro/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.bepro;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager; //앱 fragment에서 작업을 추가, 삭제, 교체하고 백 스택에 추가하는 클래스
+    private FragmentTransaction transaction; //fragment 변경을 위한 트랜잭션(작업단위)
+    private Home mHome;
+    private Recipe mRecipe;
+    private MyPage mMyPage;
+    private Notice mNotice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView mNavView = findViewById(R.id.navigation);
+        mNavView.setOnNavigationItemSelectedListener(new ItemSelectedListener()); //BottomNavigationView에 이벤트 리스너 연결
+
+        mHome = new Home(); //fragment 객체 생성
+        mRecipe = new Recipe();
+        mMyPage = new MyPage();
+        mNotice = new Notice();
+
+        fragmentManager = getSupportFragmentManager();
+
+        transaction = fragmentManager.beginTransaction(); //트랜잭션 시작
+        transaction.replace(R.id.frameLayout, mHome).commitAllowingStateLoss();
+
+
+    }
+
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull @org.jetbrains.annotations.NotNull MenuItem item) {
+            transaction = fragmentManager.beginTransaction();
+
+            switch (item.getItemId()) {
+                case R.id.home:
+                    transaction.replace(R.id.frameLayout, mHome).commitAllowingStateLoss();
+                    break;
+                case R.id.recipe:
+                    transaction.replace(R.id.frameLayout, mRecipe).commitAllowingStateLoss();
+                    break;
+                case R.id.add:
+                    //기능추가
+                    break;
+                case R.id.myPage:
+                    transaction.replace(R.id.frameLayout, mMyPage).commitAllowingStateLoss();
+                    break;
+                case R.id.notice:
+                    transaction.replace(R.id.frameLayout, mNotice).commitAllowingStateLoss();
+                    break;
+            }
+            return false;
+        }
+    }
+
+
+}
