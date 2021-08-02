b7f72c052fcea9d6932ea3104d758227f7e1ec9a
오수현 <44641501+dhtngus4921@users.noreply.github.com>
2021-08-03 00:28:22 +0900

    저장 경로 확인 !
    C:\Bitnami\wampstack-8.0.8-0\apache2\htdocs

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost:3307", "root", "123456", "test");
+    $con = mysqli_connect("localhost:3307", "beProAdmin", "123456", "bepro");
     if(mysqli_connect_errno()){
         echo("db 연결 실패 : ". mysqli_connect_error());
     }
@@ -7,21 +7,27 @@
     $userEmail = $_POST["userEmail"];
     $userPassword = $_POST["userPassword"];
 
-    $statement = mysqli_prepare($con, "SELECT * FROM users WHERE user_email = ? AND user_password = ?");
+    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE user_email = ? AND user_password = ?");
     mysqli_stmt_bind_param($statement, "ss", $userEmail, $userPassword);
     mysqli_stmt_execute($statement);
 
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $userID, $userEmail, $userPassword);
+    mysqli_stmt_bind_result($statement, $userIDX, $userID, $userPassword, $userNickname, $userImg, $userDate, $userUpdate, $userAuthority, $userEmail);
 
     $response = array();
     $response["success"] = false;
 
     while(mysqli_stmt_fetch($statement)){
         $response["success"] = true;
+        $response["userIDX"] = $userIDX;
         $response["userID"] = $userID;
-        $response["userEmail"] = $userEmail;
         $response["userPassword"] = $userPassword;
+        $response["userNickname"] = $userNickname;
+        $response["userImg"] = $userImg;
+        $response["userDate"] = $userDate;
+        $response["userUpdate"] = $userUpdate;
+        $response["userAuthority"] = $userAuthority;
+        $response["userEmail"] = $userEmail;
     }
 
     echo json_encode($response);
