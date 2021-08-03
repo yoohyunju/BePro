33f9ba0d327dce6f36c38be7e85b215713268aa7
오수현 <44641501+dhtngus4921@users.noreply.github.com>
2021-08-03 19:25:36 +0900

    저장 경로 확인 !
    > C:\Bitnami\wampstack-8.0.8-0\apache2\htdocs

---
+++ register.php
@@ -0,0 +1,21 @@
+<?php
+    $con = mysqli_connect("localhost:3307", "beProAdmin", "123456", "bepro");
+    if(mysqli_connect_errno()){
+        echo("db 연결 실패 : ". mysqli_connect_error());
+    }
+    mysqli_set_charset($con, "utf8");
+
+    $userNICK = $_POST["userNick"];
+    $userPWD = $_POST["userPassword"];
+    $userEMAIL = $_POST["userEmail"];
+    
+    $statement = mysqli_prepare($con, "INSERT INTO user (user_nickname, user_password, user_email) VALUES (?,?,?)");
+    $bind = mysqli_Stmt_bind_param($statement, "sss", $userNICK, $userPWD, $userEMAIL);
+    
+    mysqli_stmt_execute($statement);
+
+    $response = array();
+    $response["success"] = true;
+
+    echo json_encode($response);
+?>
