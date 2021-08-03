93c30a3deed0dbfe88d9394bb8c4b7ea80d6826a
오수현 <44641501+dhtngus4921@users.noreply.github.com>
2021-08-03 19:20:59 +0900

    회원가입 세부기능 추가
    > 별명/이메일 중복확인
    > 인증코드 초카운트 수정
    > edittext null 값 체크

--- app/src/main/java/com/example/bepro/CreateMemActivity.java
+++ app/src/main/java/com/example/bepro/CreateMemActivity.java
@@ -2,6 +2,7 @@ package com.example.bepro;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.RequestQueue;
@@ -18,17 +20,32 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.mail.MessagingException;
 import javax.mail.SendFailedException;
 
 public class CreateMemActivity extends AppCompatActivity {
     EditText nick, pwd, checkpwd, email, code;
-    Button check, register, codecheck;
+    Button check, register, codecheck, nickCheck;
+
     String GmailCode;
     static int value;
     int mailSend = 0;
     MainHandler mainHandler;
-    //sns 연동, db 연동
+
+    private boolean Nickvalidate = false;
+    private boolean Emailvalidate = false;
+    private boolean EmailCheck = false;
+
+    private AlertDialog dialog;
+    private CountDownTimer countDownTimer;
+    private int time = 59;
+    int min = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,9 +61,46 @@ public class CreateMemActivity extends AppCompatActivity {
         pwd = findViewById(R.id.memPwd);
         checkpwd = findViewById(R.id.checkPwd);
         register = findViewById(R.id.createMembtn);
+        nickCheck = findViewById(R.id.nickCheck);
 
+        nickSetting();
         inputSetting();
         buttonSetting();
+    }
+
+    //별명 중복 확인 버튼
+    public void nickSetting(){
+        nickCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userNick = nick.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if(success){
+                                Toast.makeText(getApplicationContext(), "사용 가능한 별명 입니다.", Toast.LENGTH_SHORT).show();
+                                nick.setEnabled(false);
+                                Nickvalidate = true;
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(CreateMemActivity.this);
+                                dialog = builder.setMessage("이미 등록된 별명입니다.").setNegativeButton("확인", null).create();
+                                dialog.show();
+                                nick.setText(null);
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                NickRequest nickRequest = new NickRequest(userNick, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(CreateMemActivity.this);
+                queue.add(nickRequest);
+            }
+        });
     }
 
     //이메일 중복 체크 버튼
@@ -54,22 +108,67 @@ public class CreateMemActivity extends AppCompatActivity {
         check.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MailThread mailThread = new MailThread();
-                mailThread.start();
+                String userEmail = email.getText().toString();
 
-                if(mailSend == 0){
-                    value = 180;
-                    BackgroundThread backgroundThread = new BackgroundThread();
-                    backgroundThread.start();
-                    mailSend += 1;
-                }else{
-                    value = 180;
+                //입력을 안했을 경우
+                if(userEmail.equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CreateMemActivity.this);
+                    dialog = builder.setMessage("이메일을 입력해주세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
                 }
-                Toast.makeText(getApplicationContext(), "인증번호를 확인하세요.", Toast.LENGTH_SHORT).show();
-                code.setVisibility(View.VISIBLE);
-                codecheck.setVisibility(View.VISIBLE);
 
-                mainHandler = new MainHandler();
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+
+                            //이메일 중복검사 & 형식 검사 조건문
+                            if(success){
+                                if(isValidEmail(userEmail)){
+                                    Toast.makeText(getApplicationContext(), "사용할 수 있는 아이디 입니다.", Toast.LENGTH_SHORT).show();
+                                    email.setEnabled(false);
+                                    Emailvalidate = true;
+                                    MailThread mailThread = new MailThread();
+                                    mailThread.start();
+
+                                    if(mailSend == 0){
+                                        value = 180;
+                                        BackgroundThread backgroundThread = new BackgroundThread();
+                                        backgroundThread.start();
+                                        mailSend += 1;
+                                    }else{
+                                        value = 180;
+                                    }
+
+                                    Toast.makeText(getApplicationContext(), "인증번호가 전송되었습니다.", Toast.LENGTH_SHORT).show();
+                                    code.setVisibility(View.VISIBLE);
+                                    codecheck.setVisibility(View.VISIBLE);
+                                    mainHandler = new MainHandler();
+                                }else{
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(CreateMemActivity.this);
+                                    dialog = builder.setMessage("유효하지 않은 이메일 형식입니다.").setNegativeButton("확인", null).create();
+                                    dialog.show();
+                                    email.setText(null);
+                                    return;
+                                }
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(CreateMemActivity.this);
+                                dialog = builder.setMessage("이미 존재하는 이메일입니다.").setNegativeButton("확인", null).create();
+                                dialog.show();
+                                email.setText(null);
+                            }
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                ValidateRequest validateRequest = new ValidateRequest(userEmail, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(CreateMemActivity.this);
+                queue.add(validateRequest);
+
             }
         });
 
@@ -78,8 +177,11 @@ public class CreateMemActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(code.getText().toString().equals(GmailCode)){
                     Toast.makeText(getApplicationContext(), "인증되었습니다.", Toast.LENGTH_SHORT).show();
+                    EmailCheck = true;
                 }else{
-                    Toast.makeText(getApplicationContext(), "인증번호를 다시 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CreateMemActivity.this);
+                    dialog = builder.setMessage("인증번호가 일치하지 않습니다.").setNegativeButton("확인", null).create();
+                    dialog.show();
                 }
             }
         });
@@ -93,6 +195,33 @@ public class CreateMemActivity extends AppCompatActivity {
                 String userNICK = nick.getText().toString();
                 String userPWD = pwd.getText().toString();
                 String userEMAIL = email.getText().toString();
+                String PWDCheck = checkpwd.getText().toString();
+
+                if(!Nickvalidate){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CreateMemActivity.this);
+                    dialog = builder.setMessage("닉네임 입력을 확인해주세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+                if(!Emailvalidate){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CreateMemActivity.this);
+                    dialog = builder.setMessage("이메일 입력을 확인해주세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+                if(!EmailCheck){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CreateMemActivity.this);
+                    dialog = builder.setMessage("인증코드 입력을 확인해주세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
+
+                if(userEMAIL.equals("") || userPWD.equals("") || userNICK.equals("")){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CreateMemActivity.this);
+                    dialog = builder.setMessage("모두 입력해주세요.").setNegativeButton("확인", null).create();
+                    dialog.show();
+                    return;
+                }
 
                 //중복, 필수 입력 검사, 이메일 인증? 추가(수정)
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
@@ -101,12 +230,19 @@ public class CreateMemActivity extends AppCompatActivity {
                         try{
                             JSONObject jsonObject = new JSONObject(response);
                             boolean success = jsonObject.getBoolean("success");
-                            if(success){
-                                Toast.makeText(getApplicationContext(), "회원 등록에 성공했습니다.", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(CreateMemActivity.this, LoginActivity.class);
-                                startActivity(intent);
+                            if(userPWD.equals(PWDCheck)){
+                                if(success){
+                                    Toast.makeText(getApplicationContext(), "회원 등록에 성공했습니다.", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(CreateMemActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+                                }else{
+                                    Toast.makeText(getApplicationContext(), "회원 등록에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                                }
                             }else{
-                                Toast.makeText(getApplicationContext(), "회원 등록에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                                AlertDialog.Builder builder = new AlertDialog.Builder(CreateMemActivity.this);
+                                dialog = builder.setMessage("비밀번호가 일치하지 않습니다.").setNegativeButton("확인", null).create();
+                                dialog.show();
+                                return;
                             }
                         }catch (JSONException e){
                             e.printStackTrace();
@@ -119,6 +255,15 @@ public class CreateMemActivity extends AppCompatActivity {
             }
         });
     }
+
+    public static boolean isValidEmail(String email) {
+        boolean err = false; String regex = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(email);
+        if(m.matches()) { err = true; }
+        return err;
+    }
+
 
     class MailThread extends Thread{
         public void run(){
@@ -164,19 +309,35 @@ public class CreateMemActivity extends AppCompatActivity {
         @Override
         public void handleMessage(Message massage){
             super.handleMessage(massage);
-            int min, sec;
-
             Bundle bundle = massage.getData();
-            int value = bundle.getInt("value");
 
-            min = value/60;
-            sec = value%60;
-
-            if(sec<10){
-                code.setHint("0"+min+" : 0"+sec);
-            }else{
-                code.setHint("0"+min+" : 0"+sec);
-            }
+            Timer timer = new Timer();
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    if(time<10){
+                        code.setHint("0" + min +" : 0" + time);
+                        time--;
+                    }else{
+                        code.setHint("0" + min +" : " + time);
+                        time--;
+                    }
+                    if(time < 0){
+                        min--;
+                        time = 59;
+                    }
+                    if(min==0 && time==0){
+                        CreateMemActivity.this.runOnUiThread(new Runnable() {
+                            public void run() {
+                                Toast.makeText(CreateMemActivity.this, "인증번호가 입력되지 않았습니다.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(CreateMemActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }
+                        });
+                    }
+                }
+            };
+            timer.schedule(task, 0000);
         }
     }
 }
---
+++ app/src/main/java/com/example/bepro/ValidateRequest.java
@@ -0,0 +1,25 @@
+package com.example.bepro;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidateRequest  extends StringRequest {
+    final static private String URL = "http://192.168.0.17:81/validate.php";
+    private Map<String, String> map;
+
+    public ValidateRequest(String UserEmail, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("userEmail", UserEmail);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError{
+        return map;
+    }
+}
