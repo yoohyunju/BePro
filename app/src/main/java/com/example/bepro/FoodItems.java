542de6791b069f80008b7b12ae60189364033f6c
yoohyunju <yhj2756@naver.com>
2021-08-11 17:00:24 +0900

    FoodAdapter 및 FoodItems 수정
    FoodAdapter 및 FoodItems 수정

--- app/src/main/java/com/example/bepro/FoodItems.java
+++ app/src/main/java/com/example/bepro/FoodItems.java
@@ -3,12 +3,24 @@ package com.example.bepro;
 public class FoodItems {
     private String foodName; //식품명
     private String foodExpiryDate; //식품 유통기한
-    private String remainDate; //유통기한 남은 날짜
+    private String foodRemainDate; //유통기한 남은 날짜
+    private int foodTotalCount; //식품 개수
 
-    public FoodItems(String foodName, String foodExpiryDate, String remainDate) {
+    public FoodItems(String foodName, String foodExpiryDate) {
         this.foodName = foodName;
         this.foodExpiryDate = foodExpiryDate;
-        this.remainDate = remainDate;
+    }
+
+    public FoodItems(String foodName, String foodExpiryDate, String foodRemainDate) {
+        this.foodName = foodName;
+        this.foodExpiryDate = foodExpiryDate;
+        this.foodRemainDate = foodRemainDate;
+    }
+
+    public FoodItems(String foodName, String foodRemainDate, int foodTotalCount) {
+        this.foodName = foodName;
+        this.foodRemainDate = foodRemainDate;
+        this.foodTotalCount = foodTotalCount;
     }
 
     public String getFoodName() {
@@ -27,11 +39,19 @@ public class FoodItems {
         this.foodExpiryDate = foodExpiryDate;
     }
 
-    public String getRemainDate() {
-        return remainDate;
+    public String getFoodRemainDate() {
+        return foodRemainDate;
     }
 
-    public void setRemainDate(String remainDate) {
-        this.remainDate = remainDate;
+    public void setFoodRemainDate(String foodRemainDate) {
+        this.foodRemainDate = foodRemainDate;
+    }
+
+    public int getFoodTotalCount() {
+        return foodTotalCount;
+    }
+
+    public void setFoodTotalCount(int foodTotalCount) {
+        this.foodTotalCount = foodTotalCount;
     }
 }
