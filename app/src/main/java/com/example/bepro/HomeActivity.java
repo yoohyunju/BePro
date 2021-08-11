7bbefdb1b86bf6ab25426f51bed30d95864d80d4
yoohyunju <yhj2756@naver.com>
2021-08-11 17:45:00 +0900

    HomeActivity 수정
    HomeActivity 수정

--- app/src/main/java/com/example/bepro/HomeActivity.java
+++ app/src/main/java/com/example/bepro/HomeActivity.java
@@ -24,13 +24,14 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+/*
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
-
+*/
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
