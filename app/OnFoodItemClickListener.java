04c7810c08288f20ac904ccc9120fc8cfed4bd84
Da-bin-kim <76606378+Da-bin-kim@users.noreply.github.com>
2021-09-20 20:45:26 +0900

    Delete OnFoodItemClickListener.java

--- app/OnFoodItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.bepro.home;
-
-import android.view.View;
-
-import com.example.bepro.home.FoodAdapter;
-
-public interface OnFoodItemClickListener {
-    public void onItemClick(FoodAdapter.ViewHolder holder, View view, int position);
-}
