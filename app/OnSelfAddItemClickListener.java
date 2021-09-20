6a4ebd49351ead3c43ec2b2c1865a5e345002360
Da-bin-kim <76606378+Da-bin-kim@users.noreply.github.com>
2021-09-20 20:46:07 +0900

    Delete OnSelfAddItemClickListener.java

--- app/OnSelfAddItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.bepro.home;
-
-import android.view.View;
-
-public interface OnSelfAddItemClickListener {
-    public void onItemClick(SelfAddItemAdapter.ViewHolder holder, View view, int position);
-}
